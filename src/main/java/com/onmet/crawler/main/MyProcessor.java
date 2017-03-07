952d01a0de378f55c4c6b5607676d38d5b41b194
liqiao <liqiao@onemt.com.cn>
2017-03-07 17:54:35 +0800

    M#crawler#更新

--- src/main/java/com/onmet/crawler/main/MyProcessor.java
+++ src/main/java/com/onmet/crawler/main/MyProcessor.java
@@ -1,8 +1,7 @@
 package com.onmet.crawler.main;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
@@ -11,11 +10,14 @@ import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.selector.JsonPathSelector;
 
+import com.onmet.crawler.pojo.PageArticle;
+import com.onmet.crawler.pojo.PageImage;
+import com.onmet.crawler.pojo.PageVideo;
+
 public class MyProcessor implements PageProcessor{
 
 	private Site site=Site.me()
 			//.setDomain("www.youtube.com")
-			.setDomain("news.baidu.com")
 			.setUserAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
 			.setTimeOut(30000)
 			.setCycleRetryTimes(3);
@@ -28,54 +30,129 @@ public class MyProcessor implements PageProcessor{
 	@Override
 	public void process(Page page) {
 		String url = page.getRequest().getUrl();
-		if(url.matches("^https://www.googleapis.com/youtube/v3/search?.*")){
+		
+		//1. 获取youtube 返回的视频列表
+		if(url.contains("https://www.googleapis.com/youtube/v3/search")){
+			//抽取视频id
 			List<String> items = page.getJson().jsonPath("$.items[*]").all();
+			String apiKey = (String) page.getRequest().getExtra("apiKey");
 			for (String item : items) {
 				String videoId = new JsonPathSelector("$.id.videoId").select(item);
 				Request request=new Request();
 				request.setUrl("https://www.youtube.com/get_video_info?video_id="+videoId);
+				request.putExtra("apiKey", apiKey);
+				request.putExtra("videoId", videoId);
 				page.addTargetRequest(request);
 				System.out.println(item);
 			}
-			page.setSkip(true);
+			//skip不会进入MyPipeline进行数据保存
+			page.setSkip(true); 
 		}
 		
-		if(url.matches("^https://www.youtube.com/get_video_info?.*")){
+		//2.判定视频是否可用
+		if(url.contains("https://www.youtube.com/get_video_info")){
 			String str = page.getRawText();
-			if(str.contains("errorcode")){
-				page.setSkip(true);
-			}
+			page.setSkip(true);
+			//存在errorcode,表示该视频无法播放
+			if(str.contains("errorcode"))
+				return;
+			
+				
+			
+			String apiKey = (String) page.getRequest().getExtra("apiKey");
+			String videoId = (String) page.getRequest().getExtra("videoId");
+			//https://www.googleapis.com/youtube/v3/videos?id=scWpXEYZEGk&part=id,contentDetails,snippet,statistics&key=AIzaSyAPjmHyYPp9iy4mNhWwhHtIYX-ZfcdG2fE
+			StringBuilder sb=new StringBuilder("https://www.googleapis.com/youtube/v3/videos?");
+			sb.append("id="+videoId)
+			.append("&key="+apiKey)
+			.append("&part=snippet,statistics");
+			//.append("&fields=items(snippet(title,publishedAt,description,thumbnails(maxres(url,width,height))),statistics,fileDetails(durationMs,creationTime))");
+			Request request=new Request();
+			request.setUrl(sb.toString());
+			request.putExtra("apiKey", apiKey);
+			request.putExtra("videoId", videoId);
+			request.setMethod("GET");
+			page.addTargetRequest(request);
 		}
 		
-		/*String rawText = page.getRawText();
-		Pattern p=Pattern.compile("cpOptions_1.data.push\\(\\{[^\\{]*\"imgUrl\": \"(.*?)\"[^\\}]*\\}\\);");
-		Matcher matcher = p.matcher(rawText);
-		while(matcher.find()){
-			System.out.println(matcher.group(1));
-		}*/
+		//3.获取视频的详细信息封装到pageArticle对象中
+		//https://developers.google.com/youtube/v3/docs/videos
+		if(url.contains("https://www.googleapis.com/youtube/v3/videos")){
+			//封装个pageArticle对象
+			String videoId = (String) page.getRequest().getExtra("videoId");
+			String rawText = page.getRawText();
+			
+			
+			String title = new JsonPathSelector("$.items[0].snippet.title").select(rawText);
+			String publishedAt = new JsonPathSelector("$.items[0].snippet.publishedAt").select(rawText);
+			String description = new JsonPathSelector("$.items[0].snippet.description").select(rawText);
+			String imageUrl =new JsonPathSelector("$.items[0].snippet.thumbnails.default.url").select(rawText);
+			if(rawText.contains("standard")){
+				imageUrl= new JsonPathSelector("$.items[0].snippet.thumbnails.standard.url").select(rawText);
+			}
+			if(rawText.contains("maxres")){
+				imageUrl = new JsonPathSelector("$.items[0].snippet.thumbnails.maxres.url").select(rawText);
+			}
+			//String durationMs = new JsonPathSelector("$.items[0].fileDetails.durationMs").select(rawText);
+			PageArticle pageArticle=new PageArticle();
+			pageArticle.setDate(publishedAt);
+			pageArticle.setDiscussion(description);
+			pageArticle.setHumanLanguage("ar");
+			pageArticle.setText(description);
+			pageArticle.setTitle(title);
+			pageArticle.setType("video");
+			
+			List<PageImage> pageImages=new ArrayList<>();
+			PageImage pageImage=new PageImage();
+			pageImage.setPrimary(true);
+			pageImage.setUrl(imageUrl);
+			pageImages.add(pageImage);
+			
+			List<PageVideo> pageVideos=new ArrayList<>();
+			PageVideo pageVideo=new PageVideo();
+			pageVideo.setPrimary(true);
+			pageVideo.setUrl("https://www.youtube.com/embed/"+videoId);
+			pageVideos.add(pageVideo);
+			
+			pageArticle.setVideos(pageVideos);
+			pageArticle.setImages(pageImages);
+			
+			page.putField("pageArticle", pageArticle);
+			
+		}
+		
+		
 			
 		
 	}
 	
 	
 	public static void main(String[] args) {
+		
+		//构造url
 		StringBuilder sb=new StringBuilder();
 		sb.append("https://www.googleapis.com/youtube/v3/search?key=AIzaSyAPjmHyYPp9iy4mNhWwhHtIYX-ZfcdG2fE")
 		.append("&relevanceLanguage=ar")
 		.append("&topicId=/m/02vx4")
 		.append("&type=video")
 		.append("&maxResults=5")
-		.append("&part=id,snippet")
+		.append("&part=id")
 		.append("&videoDefinition=high")
 		.append("&order=viewCount")
 		.append("&safeSearch=strict")
+		.append("&videoLicense=youtube")
+		.append("&videoSyndicated=true")
 		.append("&fields=*");
+		
+		Request request=new Request();
+		request.setUrl(sb.toString());
+		//模拟带参数
+		request.putExtra("apiKey", "AIzaSyAPjmHyYPp9iy4mNhWwhHtIYX-ZfcdG2fE");
 		Spider.create(new MyProcessor())
-		//.addPipeline(pipeline)
-		.addUrl(sb.toString())
-		.thread(5)
-		.runAsync();
-		//Spider.create(new MyProcessor()).addUrl("http://news.baidu.com").thread(1).runAsync();
+		.addRequest(request)
+		.addPipeline(new MyPipeline())
+		.thread(10)
+		.run();
 		
 	}
 }
